61b8715a55692b5b30d2664a3b0d352de28f2892
luke.bohonis <w.bohonis@gmail.com>
2020-01-15 12:56:17 -0800

    Add welcome pages and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+  get 'welcome/about'
+  get 'welcome/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'welcome#home'
-  get '/about', to: 'welcome#about'
+  get '/', to: 'welcome#home', as: 'home'
+  get '/about', to: 'welcome#about', as: 'about'
+  get '/contact', to: 'welcome#contact', as: 'contact'
+  post '/contact', to: 'welcome#contact'
 end
