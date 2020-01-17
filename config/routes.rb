8479078f29ff15c7abec0bb8cb58ba6416f4777c
luke.bohonis <w.bohonis@gmail.com>
2020-01-16 17:49:20 -0800

    Add contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/home'
-  get 'welcome/about'
-  get 'welcome/contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#home', as: 'home'
   get '/about', to: 'welcome#about', as: 'about'
   get '/contact', to: 'welcome#contact', as: 'contact'
-  post '/contact', to: 'welcome#contact'
+  post '/contact', to: 'welcome#thanks', as: 'thanks'
 end
